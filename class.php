06cb9e44aeef8f9dfc6f4fd7042056c5eb1b0cb8
DEVLOG.E01 <DEVLOG.E01@afpar.com>
2021-12-03 09:10:30 +0400

    ajout de class.php

---
+++ class.php
@@ -0,0 +1,22 @@
+<?php
+class Connexion {
+
+}
+
+class User {
+    public $nom;
+    public $mail;
+    public $password;
+    public $droit;
+
+    public function __construct($nom, $mail, $password, $droit) {
+        $this-> nom=$nom;
+        $this->mail=$mail;
+        $this->password=$password;
+        $this->droit=$droit;
+    } 
+    public function enregistrerDansLaBdd () {
+
+    }
+}
+?>
