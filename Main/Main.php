370b06332da5a652fb72215928a56ae2486818d7
DEVLOG.E01 <DEVLOG.E01@afpar.com>
2021-12-22 07:50:54 +0400

    restriction de l'acces de gestion article aux utilisateurs ayant le droit d'admin via le main

--- Main/Main.php
+++ Main/Main.php
@@ -1,12 +1,18 @@
 <?php
-    /*
+/*
         Cette classe représente le routeur principal
         Elle va aller chercher et lire les URL et renvoyer vers le Controlleur concerné
     */
-namespace App\Main;
-use App\Controllers;
 
-class Main {
+namespace App\Main;
+
+use App\Controllers;
+use App\Controllers\Controller;
+use App\Controllers\ConnectionController;
+
+class Main
+{
+
     /*********************C'est mon router, un "router simple qui ne gère pas l'erreur*********************/
     /*
     Exemple du fonctionnement prévu pour les url
@@ -19,10 +25,11 @@ class Main {
     Adresse réelle 
         http://mes-articles.test/index.php?p=articles/details/parametre
 */
-    
-    public function start(){
 
-/*******NETTOYAGE D'URL ET ON EVITE LE DUPLICATE CONTENT ***********/
+    public function start()
+    {
+
+        /*******NETTOYAGE D'URL ET ON EVITE LE DUPLICATE CONTENT ***********/
 
         //On va retirer le "trailing slash" éventuel de l'Url
         // On récupère l'adresse url
@@ -33,7 +40,7 @@ class Main {
             on aura la même page avec ou sans le slash, ça fait ce que l'on appelle du duplicate content
         */
 
-        if(!empty($uri) && $uri !='/' && $uri[-1] === '/'){ //il y a quand même un slash systématique à la fin paraît-il, au moins sur la page d'accueil
+        if (!empty($uri) && $uri != '/' && $uri[-1] === '/') { //il y a quand même un slash systématique à la fin paraît-il, au moins sur la page d'accueil
             // Dans ce cas on enlève le /
             $uri = substr($uri, 0, -1);
 
@@ -41,11 +48,11 @@ class Main {
             http_response_code(301);
 
             // On redirige vers l'URL sans /
-            header('Location: '.$uri);
+            header('Location: ' . $uri);
             exit;
-        }   
+        }
 
-/*******GESTION DES PARAMETRES**********/
+        /*******GESTION DES PARAMETRES**********/
 
         /* 
         On sépare les paramètres dans un tableau 
@@ -54,52 +61,66 @@ class Main {
         c'est aussi pour ne pas avoir une positition de $params qui stocke un slash 
         qu'on l'a enlevé dans le if d'avant
         */
-        $params=[];
-        if (isset($_GET['p'])){
+        $params = [];
+        if (isset($_GET['p'])) {
             $params = explode('/', $_GET['p']);
-         }
+        }
 
         // On vérifie si on a au moins un paramètre (donc si il y en a un dans $params[0])
-        if($params[0] != ""){
-        /*
+        if ($params[0] != "") {
+            /*
         PARAM 1    
             Le 1er param va correspondre au nom du controller, mais il va falloir refabriquer l'adresse du ficihier (avec le namespace)
             On sauvegarde le 1er paramètre dans $controller en mettant sa 1ère lettre en majuscule
             + en ajoutant le namespace des controleurs, + ajoutant "Controller" à la fin
             Ex : pour monsite/bidule en url ; on veut qu'il aille chercher App\Controller\BiduleController
         */
-            $controller = '\\App\\Controllers\\'.ucfirst(array_shift($params)).'Controller'; //array_shift enlève la première valeur d'un tableau
+
+        // Je contrôle si l'utilisateur est admin avant de continuer. Toutes les fonctions de cette classes renvoient vers des pages destinées exclusivement à l'admin
+            if (strcasecmp($params[0],'gestionarticles')==0) { //strcasecmp pour ignorer la casse
+   
+                $instanceConnectionController = new ConnectionController;
+                if ($instanceConnectionController->isTheUserAnAdmin() == false) {
+                    http_response_code(404);
+                    $instanceConnectionController->render('404');
+                    exit;
+                }
+            } 
+
+            $controller = '\\App\\Controllers\\' . ucfirst(array_shift($params)) . 'Controller'; //array_shift enlève la première valeur d'un tableau
             // On instancie le contrôleur
             $controller = new $controller();
 
-        /* 
+            /* 
         PARAM 2
             On sauvegarde le 2ème paramètre dans $action 
             si on a un param, c'est une méthode qu'on utilise -> soit elle existe, soit 404
             si pas de param, on utilise la méthode index
         */
-            $action = isset($params[0]) ? array_shift($params) : 'index';   
+            $action = isset($params[0]) ? array_shift($params) : 'index';
 
-            if(method_exists($controller, $action)){
-            /* 
+            if (method_exists($controller, $action)) {
+                /* 
                 Si il reste des paramètres : 
                 on appelle la méthode en envoyant les paramètres 
                 sinon on l'appelle "à vide"
             */
-            /*    
+
+                /*    
                 ancienne version :  (isset($params[0])) ? $controller->$action($params) : $controller->$action();
                 cela permettait de passer des paramètres complémentaires sous forme de un tableau
                 maintenant -> on veut un entier directement. call_user_func_array permet de faire la conversion
             */
-                (isset($params[0])) ? call_user_func_array([$controller,$action], $params) : $controller->$action(); 
-
-            }else{
+                (isset($params[0])) ? call_user_func_array([$controller, $action], $params) : $controller->$action();
+            } else {
                 /*
                     si il y a un param de méthode mais que cette méthode n'existe pas, 
                     on envoie le code réponse 404
                 */
                 http_response_code(404);
-                echo "La page recherchée n'existe pas";
+                $instanceController = new ConnectionController; 
+                $instanceController->render('404');
+                exit;
             }
         } else {
             /*
@@ -111,8 +132,5 @@ class Main {
             // On appelle la méthode index
             $controller->index();
         }
-
-
     }
 }
-?>
---
+++ public/views/404.php
@@ -0,0 +1,2 @@
+<h2>Erreur 404 : soit cette page n'existe pas, soit vous n'y avez pas accès</h2>
+<h5 class="blanc">Nous vous suggérons de vous connecter ou d'aller vous promener ailleurs</h5>
